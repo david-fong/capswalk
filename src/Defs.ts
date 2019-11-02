e9e214415cfe90a6c1a5b0d6af4cfa6676b50ec6
david-fong <davidfong19@gmail.com>
2019-11-02 15:31:02 -0700

    update project meta files for node modules

---
+++ src/Defs.ts
@@ -0,0 +1,13 @@
+
+/**
+ * 
+ */
+class Defs {
+
+    /**
+     * The maximum possible number of unoccupied twice-neighbouring
+     * `Tile`s for any given `Tile` by this square `Grid` style.
+     */
+    public static readonly MAX_NUM_U2NTS = (((2*2) + 1) ** 2) - 1;
+
+}
