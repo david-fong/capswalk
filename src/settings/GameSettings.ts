4263fcea642b7fccb197d274f84124b277a43c7b
david-fong <davidfong19@gmail.com>
2019-11-12 13:15:11 -0800

    move settings to dedicated folder

---
+++ src/settings/GameSettings.ts
@@ -0,0 +1,24 @@
+import { Sound } from "./Sound";
+
+
+/**
+ * Included Settings:
+ * - Volume / Mute
+ * - Color theme
+ */
+export class GameNonPrivilegedSettings {
+
+    public readonly sound: Sound;
+
+}
+
+
+
+/**
+ * Included Settings:
+ * - Spice button
+ * - Difficulty
+ */
+export class GamePrivilegedSettings {
+
+}
---
+++ src/settings/Sound.ts
@@ -0,0 +1,11 @@
+
+/**
+ * 
+ */
+export class Sound {
+
+    public constructor() {
+
+    }
+
+}
