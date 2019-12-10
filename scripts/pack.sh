8abd83b4d87ece83dd8096a64605b6e687ce1e8a
david-fong <davidfong19@gmail.com>
2019-12-09 20:47:41 -0800

    change pack script to output webpack config to project root

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-declare -r scripts="$(dirname "${BASH_SOURCE[0]}")"
-npx tsc --outDir "${scripts}" "${scripts}/../webpack.config.ts"
-npx webpack --color --config "${scripts}/webpack.config.js"
+declare -r cwd="$(dirname "${BASH_SOURCE[0]}")"
+npx tsc --outDir "${cwd}/.." "${cwd}/../webpack.config.ts"
+npx webpack --color --config "${cwd}/../webpack.config.js"
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,8 @@
-const path = require('path');
-import * as webpack from 'webpack';
+const path = require("path");
+
+// only used for type-hinting.
+// omitted from transpilation output.
+import * as webpack from "webpack";
 
 /**
  * https://www.typescriptlang.org/docs/handbook/react-&-webpack.html
@@ -8,22 +11,24 @@ import * as webpack from 'webpack';
 const config: webpack.Configuration = {
     // https://webpack.js.org/configuration/entry-context/#context
     context: __dirname,
-    entry: './src/index.ts',
+    entry: "./src/index.ts",
+    devtool: "source-map",
+    resolve: {
+        extensions: [ ".ts", ".tsx", ".js", ],
+        modules: [ "src", "node_modules", ],
+    },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
+                use: "ts-loader",
                 exclude: /node_modules/,
             },
         ],
     },
-    resolve: {
-        extensions: [ '.ts', '.tsx', '.js', ],
-    },
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
     },
 };
 
