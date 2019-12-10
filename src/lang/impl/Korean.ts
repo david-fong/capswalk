7ca275c71bf8f1be83f0ddb07fd4b9bb493fc014
david-fong <davidfong19@gmail.com>
2019-12-10 13:24:53 -0800

    fill in korean jamo information

--- src/lang/impl/Korean.ts
+++ src/lang/impl/Korean.ts
@@ -116,31 +116,108 @@ export namespace Korean {
 
 
 
-    type LetterDesc = Readonly<{}>;
+    const UNICODE_HANGUL_SYLLABLES_BASE = 0xAC00;
+
+    type JamoDesc = Readonly<{
+        value: string; // not used. for readability.
+        atoms: string; // used for keyboard mapping.
+        roman: string;
+    }>;
+
+    type Syllable = Readonly<{
+        initial: JamoDesc;
+        medial:  JamoDesc;
+        final:   JamoDesc;
+    }>;
 
     /**
-     * Choseong
+     * # Initial Jamo (Choseong)
      */
     const INITIALS = Object.freeze(<const>[
-        {},
+        { value: "ㄱ", atoms: "ㄱ",   roman: "", },
+        { value: "ㄲ", atoms: "ㄱㄱ", roman: "", },
+        { value: "ㄴ", atoms: "ㄴ",   roman: "", },
+        { value: "ㄷ", atoms: "ㄷ",   roman: "", },
+        { value: "ㄸ", atoms: "ㄷㄷ", roman: "", },
+        { value: "ㄹ", atoms: "ㄹ",   roman: "", },
+        { value: "ㅁ", atoms: "ㅁ",   roman: "", },
+        { value: "ㅂ", atoms: "ㅂ",   roman: "", },
+        { value: "ㅃ", atoms: "ㅂㅂ", roman: "", },
+        { value: "ㅅ", atoms: "ㅅ",   roman: "", },
+        { value: "ㅆ", atoms: "ㅅㅅ", roman: "", },
+        { value: "ㅇ", atoms: "ㅇ",   roman: "", },
+        { value: "ㅈ", atoms: "ㅈ",   roman: "", },
+        { value: "ㅉ", atoms: "ㅈㅈ", roman: "", },
+        { value: "ㅊ", atoms: "ㅊ",   roman: "", },
+        { value: "ㅋ", atoms: "ㅋ",   roman: "", },
+        { value: "ㅌ", atoms: "ㅌ",   roman: "", },
+        { value: "ㅍ", atoms: "ㅍ",   roman: "", },
+        { value: "ㅎ", atoms: "ㅎ",   roman: "", },
     ]);
-    INITIALS as ReadonlyArray<LetterDesc>; // type-check
+    INITIALS as ReadonlyArray<JamoDesc>; // type-check
 
     /**
-     * Jungseong
+     * # Medial Jamo (Jungseong)
      */
     const MEDIALS = Object.freeze(<const>[
-        {},
+        { value: "ㅏ", atoms: "ㅏ",   roman: "", },
+        { value: "ㅐ", atoms: "ㅐ",   roman: "", },
+        { value: "ㅑ", atoms: "ㅑ",   roman: "", },
+        { value: "ㅒ", atoms: "ㅒ",   roman: "", },
+        { value: "ㅓ", atoms: "ㅓ",   roman: "", },
+        { value: "ㅔ", atoms: "ㅔ",   roman: "", },
+        { value: "ㅕ", atoms: "ㅕ",   roman: "", },
+        { value: "ㅖ", atoms: "ㅖ",   roman: "", },
+        { value: "ㅗ", atoms: "ㅗ",   roman: "", },
+        { value: "ㅘ", atoms: "ㅗㅏ", roman: "", },
+        { value: "ㅙ", atoms: "ㅗㅐ", roman: "", },
+        { value: "ㅚ", atoms: "ㅗㅣ", roman: "", },
+        { value: "ㅛ", atoms: "ㅛ",   roman: "", },
+        { value: "ㅜ", atoms: "ㅜ",   roman: "", },
+        { value: "ㅝ", atoms: "ㅜㅓ", roman: "", },
+        { value: "ㅞ", atoms: "ㅜㅔ", roman: "", },
+        { value: "ㅟ", atoms: "ㅜㅣ", roman: "", },
+        { value: "ㅠ", atoms: "ㅠ",   roman: "", },
+        { value: "ㅡ", atoms: "ㅡ",   roman: "", },
+        { value: "ㅢ", atoms: "ㅡㅣ", roman: "", },
+        { value: "ㅣ", atoms: "ㅣ",   roman: "", },
     ]);
-    MEDIALS as ReadonlyArray<LetterDesc>; // type-check
+    MEDIALS as ReadonlyArray<JamoDesc>; // type-check
 
     /**
-     * Jongseong
+     * # Final Jamo (Jongseong)
      */
     const FINALS = Object.freeze(<const>[
-        {},
+        { value: "",   atoms: "",     roman: "", },
+        { value: "ㄱ", atoms: "ㄱ",   roman: "", },
+        { value: "ㄲ", atoms: "ㄱㄱ", roman: "", },
+        { value: "ㄳ", atoms: "ㄱㅅ", roman: "", },
+        { value: "ㄴ", atoms: "ㄴ",   roman: "", },
+        { value: "ㄵ", atoms: "ㄴㅈ", roman: "", },
+        { value: "ㄶ", atoms: "ㄴㅎ", roman: "", },
+        { value: "ㄷ", atoms: "ㄷ",   roman: "", },
+        { value: "ㄹ", atoms: "ㄹ",   roman: "", },
+        { value: "ㄺ", atoms: "ㄹㄱ", roman: "", },
+        { value: "ㄻ", atoms: "ㄹㅁ", roman: "", },
+        { value: "ㄼ", atoms: "ㄹㅂ", roman: "", },
+        { value: "ㄽ", atoms: "ㄹㅅ", roman: "", },
+        { value: "ㄾ", atoms: "ㄹㅌ", roman: "", },
+        { value: "ㄿ", atoms: "ㄹㅍ", roman: "", },
+        { value: "ㅀ", atoms: "ㄹㅎ", roman: "", },
+        { value: "ㅁ", atoms: "ㅁ",   roman: "", },
+        { value: "ㅂ", atoms: "ㅂ",   roman: "", },
+        { value: "ㅄ", atoms: "ㅂㅅ", roman: "", },
+        { value: "ㅅ", atoms: "ㅅ",   roman: "", },
+        { value: "ㅆ", atoms: "ㅅㅅ", roman: "", },
+        { value: "ㅇ", atoms: "ㅇ",   roman: "", },
+        { value: "ㅈ", atoms: "ㅈ",   roman: "", },
+        { value: "ㅊ", atoms: "ㅊ",   roman: "", },
+        { value: "ㅋ", atoms: "ㅋ",   roman: "", },
+        { value: "ㅌ", atoms: "ㅌ",   roman: "", },
+        { value: "ㅍ", atoms: "ㅍ",   roman: "", },
+        { value: "ㅎ", atoms: "ㅎ",   roman: "", },
     ]);
-    FINALS as ReadonlyArray<LetterDesc>; // type-check
+    FINALS as ReadonlyArray<JamoDesc>; // type-check
 
     /**
      * 
