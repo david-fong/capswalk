568b830a0a8e500fe2bea57cde60de6870951af4
david-fong <davidfong19@gmail.com>
2019-12-12 13:42:24 -0800

    add English Mixed-Case language implementation

--- src/lang/impl/English.ts
+++ src/lang/impl/English.ts
@@ -2,45 +2,128 @@ import { Lang } from "src/lang/Lang";
 
 
 /**
- * # English (QWERTY)
  * 
- * https://en.wikipedia.org/wiki/Keyboard_layout#QWERTY
  */
-export class English extends Lang {
+export namespace English {
 
-    private static SINGLETON?: English = undefined;
-
-    public static getName(): string {
-        return "English (QWERTY)";
-    }
-
-    public static getBlurb(): string {
-        return ""; // TODO
-    }
-
-    public static getInstance(): English {
-        if (!(English.SINGLETON)) {
-            English.SINGLETON  = new English();
-            delete English.INITIALIZER;
-        }
-        return English.SINGLETON;
-    }
-
-    // TODO: see https://en.wikipedia.org/wiki/Keyboard_layout#Dvorak
-    // and https://en.wikipedia.org/wiki/Keyboard_layout#Colemak
     /**
-     * Change uppercase input to lowercase for convenience.
+     * # Lowercase (QWERTY)
      * 
-     * @override
+     * https://wikipedia.org/wiki/Keyboard_layout#QWERTY
      */
-    public remapKey(input: string): string {
-        return input.toLowerCase();
+    export class Lowercase extends Lang {
+
+        private static SINGLETON?: Lowercase = undefined;
+
+        public static getName(): string {
+            return "English Lowercase (QWERTY)";
+        }
+
+        public static getBlurb(): string {
+            return ""; // TODO
+        }
+
+        public static getInstance(): Lowercase {
+            if (!(Lowercase.SINGLETON)) {
+                Lowercase.SINGLETON  = new Lowercase();
+            }
+            return Lowercase.SINGLETON;
+        }
+
+        // TODO: see https://wikipedia.org/wiki/Keyboard_layout#Dvorak
+        // and https://wikipedia.org/wiki/Keyboard_layout#Colemak
+        /**
+         * Change uppercase input to lowercase for convenience.
+         * 
+         * @override
+         */
+        public remapKey(input: string): string {
+            return input.toLowerCase();
+        }
+
+        private constructor() {
+            super(
+                Lowercase.getName(),
+                Object.entries(LETTER_FREQUENCY).reduce<Lang.CharSeqPair.WeightedForwardMap>(
+                    (accumulator, current) => {
+                        const char: Lang.Char = current[0];
+                        const seq:  Lang.Seq  = current[0];
+                        const weight: number  = current[1];
+                        accumulator[char] = { seq, weight, };
+                        return accumulator;
+                    },
+                    {},
+                ),
+            );
+        }
     }
+    Lowercase as Lang.Info;
+
+
+
+    /**
+     * # Mixed-Case (QWERTY)
+     * 
+     * https://wikipedia.org/wiki/Keyboard_layout#QWERTY
+     */
+    export class MixedCase extends Lang {
+
+        private static SINGLETON?: MixedCase = undefined;
+
+        public static getName(): string {
+            return "English Mixed-Case (QWERTY)";
+        }
+
+        public static getBlurb(): string {
+            return ""; // TODO
+        }
+
+        public static getInstance(): MixedCase {
+            if (!(MixedCase.SINGLETON)) {
+                MixedCase.SINGLETON  = new MixedCase();
+            }
+            return MixedCase.SINGLETON;
+        }
+
+        /**
+         * Nothing.
+         * 
+         * @override
+         */
+        public remapKey(input: string): string {
+            return input;
+        }
+
+        private constructor() {
+            let initializer: Lang.CharSeqPair.WeightedForwardMap = {};
+            const addMappings = (charSeqTransform: (charOrSeq: string) => string): void => {
+                initializer = Object.entries(LETTER_FREQUENCY).reduce(
+                    (accumulator, current) => {
+                        const char: Lang.Char = charSeqTransform(current[0]);
+                        const seq:  Lang.Seq  = charSeqTransform(current[0]);
+                        const weight: number  = current[1];
+                        accumulator[char] = { seq, weight, };
+                        return accumulator;
+                    },
+                    initializer,
+                );
+            };
+            addMappings((cs) => cs.toLowerCase());
+            addMappings((cs) => cs.toUpperCase());
+            super(
+                MixedCase.getName(),
+                initializer,
+            );
+        }
+    }
+    MixedCase as Lang.Info;
+
+
 
     /**
      * Values obtained from https://wikipedia.org/wiki/Letter_frequency
      */
-    private static INITIALIZER = Object.freeze(<const>{
+    const LETTER_FREQUENCY = Object.freeze(<const>{
         a: 8.167, b: 1.492, c: 2.202, d: 4.253,
         e:12.702, f: 2.228, g: 2.015, h: 6.094,
         i: 6.966, j: 0.153, k: 1.292, l: 4.025,
@@ -50,21 +133,4 @@ export class English extends Lang {
         y: 1.994, z: 0.077,
     });
 
-    private constructor() {
-        super(
-            English.getName(),
-            Object.entries(English.INITIALIZER).reduce<Lang.CharSeqPair.WeightedForwardMap>(
-                (accumulator, current) => {
-                    const char: Lang.Char = current[0];
-                    const seq:  Lang.Seq  = current[0];
-                    const weight: number  = current[1];
-                    accumulator[char] = { seq, weight, };
-                    return accumulator;
-                },
-                {},
-            ),
-        );
-    }
-
 }
-English as Lang.Info;
