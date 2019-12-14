f61e9203202ca5372b8d231d2d077f93aee4386d
david-fong <davidfong19@gmail.com>
2019-12-14 10:10:29 -0800

    add webpack html and clean plugins

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 declare -r cwd="$(dirname "${BASH_SOURCE[0]}")"
+
+# I want to write the config in Typescript, but I don't want to install
+# any dependencies to run the typescript config, so we first have to
+# convert the Typescript version of the config to Javascript.
 npx tsc --build "${cwd}/webpack.tsconfig.json"
-npx webpack --verbose --progress --dispaly-error-details --color --config "${cwd}/../webpack.config.js"
+
+# --verbose
+npx webpack --config "${cwd}/../webpack.config.js"
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,9 +1,56 @@
-const path = require("path");
+import path = require("path");
+
+import webpack = require("webpack");
+import HtmlPlugin = require("html-webpack-plugin");
+import clean = require("clean-webpack-plugin");
 
 // only used for type-hinting.
 // omitted from transpilation output.
-import * as webpack from "webpack";
 import * as tsloader from "ts-loader/dist/interfaces";
+
+
+/**
+ * Relative paths resolve based off the parent directory of this file.
+ */
+const context = __dirname;
+
+const webpageEntry = <const>[
+    // "homepage"
+    "offline",
+    "server",
+    //"client",
+];
+
+/**
+ * # Entrypoints
+ */
+const entry: webpack.Entry = {
+    "test": "./test/index.ts",
+    // "test/lang"
+};
+const plugins: Array<webpack.Plugin> = [
+    new clean.CleanWebpackPlugin(),
+    new webpack.ProgressPlugin(),
+];
+
+// Add entrypoints for webpages:
+webpageEntry.forEach((name) => {
+    entry[name] = `./src/${name}/index.ts`;
+    //entry[`${name}_body`] = `./src/${name}/body.html`;
+    plugins.push(
+        new HtmlPlugin({
+            template: "./src/base/index.html",
+            filename: `${name}/index.html`,
+            chunks: [
+                name,
+                //`${name}_body`,
+            ],
+            hash: true,
+        })
+    );
+});
+
+
 
 /**
  * https://www.typescriptlang.org/docs/handbook/react-&-webpack.html
@@ -13,16 +60,44 @@ import * as tsloader from "ts-loader/dist/interfaces";
  */
 const config: webpack.Configuration = {
     mode: "development",
-    // https://webpack.js.org/configuration/entry-context/#context
-    context: __dirname,
-    entry: {
-        "main": "./src/index.ts",
-        "test": "./test/index.ts",
+    node: {
+        "fs": "empty",
+        "uws": "empty",
+        "net": "empty",
     },
+    cache: true, // https://webpack.js.org/guides/caching/
+    stats: {
+        // https://webpack.js.org/configuration/stats/
+        colors: true,
+    },
+    optimization: {
+        moduleIds: "hashed",
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+    },
+
+
+    // https://webpack.js.org/configuration/entry-context/#context
+    context,
+    entry,
     devtool: "source-map",
     resolve: {
         extensions: [ ".ts", ".json", ".tsx", ".js", ],
-        modules: [ __dirname, "node_modules", ], // .shift(path.resolve(__dirname, "src"))
+        modules: [ ".", "./node_modules", ],
+        // Note: if the ./src/ -based imports were to lose the "src/"
+        // prefixes for brevity, then we would need to add "src" after ".".
+        // If we did the same for test module imports, we would need to add
+        // "test" before or after "src" to the above list and make sure to
+        // prefix "src/" when importing source modules. Would also have to
+        // use the "paths" field to tsconfig.json to help typescript know
+        // what we are doing.
     },
     module: {
         rules: [
@@ -36,11 +111,13 @@ const config: webpack.Configuration = {
                 } as webpack.RuleSetLoader,
                 exclude: /node_modules/,
             },
+            // TODO: look into need for html loader
         ] as Array<webpack.RuleSetRule>,
     },
+    plugins,
     output: {
-        filename: "bundle-[name].js",
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(context, "dist"),
+        filename: "[name]/index.[contentHash].js",
     },
 };
 
