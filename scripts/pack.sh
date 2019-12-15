492dfc73e502956c8240283fb458745cbf80d969
david-fong <davidfong19@gmail.com>
2019-12-15 13:17:08 -0800

    custom progress report, export multiple configs for web and node targets

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
+set -e
 declare -r cwd="$(dirname "${BASH_SOURCE[0]}")"
 
 # I want to write the config in Typescript, but I don't want to install
 # any dependencies to run the typescript config, so we first have to
 # convert the Typescript version of the config to Javascript.
-npx tsc --build "${cwd}/webpack.tsconfig.json"
+if [[ "$@" =~ '-t' ]]
+then
+    time npx tsc --build "${cwd}/webpack.tsconfig.json"
+    echo 'built the webpack config'
+    echo
+else
+    echo
+    echo 'pass "-t" to first rebuild the webpack config'
+    echo
+fi
+
 
 # --verbose
-npx webpack --config "${cwd}/../webpack.config.js"
+# interesting note: if you export an array of configs, the
+# stats.color argument is ignored... I haven't fiddled to
+# see what else is like this.
+npx webpack --color --config "${cwd}/../webpack.config.js"
--- webpack.config.ts
+++ webpack.config.ts
@@ -2,107 +2,97 @@ import path = require("path");
 
 import webpack = require("webpack");
 import HtmlPlugin = require("html-webpack-plugin");
-import clean = require("clean-webpack-plugin");
+
+// Note: if I ever add this back, I'll need to look into how to make
+// sure it doesn't clean away things from separate configs (See notes
+// below on why I export multiple configurations).
+// import clean = require("clean-webpack-plugin");
+
+// https://www.npmjs.com/package/webpack-node-externals
+import nodeExternals = require("webpack-node-externals");
 
 // only used for type-hinting.
 // omitted from transpilation output.
+// https://github.com/TypeStrong/ts-loader#loader-options
 import * as tsloader from "ts-loader/dist/interfaces";
 
 
+
 /**
- * Relative paths resolve based off the parent directory of this file.
+ * Relative paths in any config fields will resolve off the parent
+ * directory of this file.
  */
-const context = __dirname;
+const PROJECT_ROOT = __dirname;
 
-const webpageEntry = <const>[
-    // "homepage"
-    "offline",
-    "server",
-    //"client",
-];
+const WATERMARK = "/**\n * " + [
+      "SnaKey by David Fong",
+      "https://github.com/david-fong/SnaKey-NTS",
+].join("\n * ") + "\n */";
 
-/**
- * # Entrypoints
- */
-const entry: webpack.Entry = {
-    "test": "./test/index.ts",
-    // "test/lang"
-};
-const plugins: Array<webpack.Plugin> = [
-    new clean.CleanWebpackPlugin(),
-    new webpack.ProgressPlugin(),
-];
-
-// Add entrypoints for webpages:
-webpageEntry.forEach((name) => {
-    entry[name] = `./src/${name}/index.ts`;
-    //entry[`${name}_body`] = `./src/${name}/body.html`;
-    plugins.push(
-        new HtmlPlugin({
-            template: "./src/base/index.html",
-            filename: `${name}/index.html`,
-            chunks: [
-                name,
-                //`${name}_body`,
-            ],
-            hash: true,
-        })
-    );
-});
-
-
+const BasePlugins: () => Array<webpack.Plugin> = () => {return [
+    new webpack.ProgressPlugin((pct, msg, moduleProgress?, activeModules?, moduleName?) => {
+        console.log(
+            `[${Math.floor(pct * 100).toString().padStart(3)}% ]`,
+            (msg === "building") ? msg : msg.padEnd(45),
+            (msg === "building") ? moduleProgress!.padStart(15) : (moduleProgress || ""),
+            (moduleName || "").replace(PROJECT_ROOT, "...").replace(PROJECT_ROOT, "..."),
+        );
+    }),
+]; };
 
 /**
- * https://www.typescriptlang.org/docs/handbook/react-&-webpack.html
- * https://webpack.js.org/configuration/configuration-languages/#typescript
+ * # Base Config
  * 
- * https://github.com/TypeStrong/ts-loader#loader-options
+ * The way my project is set up as a single node package, I need to
+ * export an array of configs. Note that I don't bundle any node
+ * modules, but if if do, there are tricky things that can be solved
+ * by adding `node: { "fs": "empty", "net": "empty", },`
+ * 
+ * Everything that builds off of this will need to add the `entry` field.
+ * 
+ * **Important**: Make sure all referenced objects are only accessible
+ * as producers. Otherwise, mutations in one bundle configurations will
+ * propagate to all following config definitions.
+ * 
+ * ## Web Bundles:
+ * 
+ * This includes the homepage, 
+ * 
+ * - `target: "web",`. This is implied, but here, explicitness helps me learn.
+ * - `externals: [ nodeExternals(), ],` or something like `[ "socket.io-client", ]`
+ * - appropriate plugin entries for the index.html file.
+ * 
+ * ## Node Bundles:
+ * 
+ * - `target: "node"`. This should mean that node modules are not bundled.
+ * - `resolve.modules.push("node_modules")`
+ * - `resolve.extensions.push(".js")`
+ * 
+ * ## Help Links
+ * 
+ * - https://www.typescriptlang.org/docs/handbook/react-&-webpack.html
+ * - https://webpack.js.org/configuration/configuration-languages/#typescript
+ * - https://github.com/TypeStrong/ts-loader#loader-options
+ * 
+ * @returns A standalone ("deep-copy") basic configuration.
  */
-const config: webpack.Configuration = {
+const BaseConfig: () => webpack.Configuration = () => { return {
     mode: "development",
-    node: {
-        "fs": "empty",
-        "uws": "empty",
-        "net": "empty",
-    },
-    cache: true, // https://webpack.js.org/guides/caching/
-    stats: {
-        // https://webpack.js.org/configuration/stats/
-        colors: true,
-    },
-    optimization: {
-        moduleIds: "hashed",
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                },
-            },
-        },
-    },
+    target: "node",
+    externals: [ nodeExternals(), ],
+    // cache: true, // https://webpack.js.org/guides/caching/
+    stats: { }, // https://webpack.js.org/configuration/stats/
 
-
-    // https://webpack.js.org/configuration/entry-context/#context
-    context,
-    entry,
+    context: PROJECT_ROOT, // https://webpack.js.org/configuration/entry-context/#context
     devtool: "source-map",
+    plugins: BasePlugins(),
     resolve: {
-        extensions: [ ".ts", ".json", ".tsx", ".js", ],
-        modules: [ ".", "./node_modules", ],
-        // Note: if the ./src/ -based imports were to lose the "src/"
-        // prefixes for brevity, then we would need to add "src" after ".".
-        // If we did the same for test module imports, we would need to add
-        // "test" before or after "src" to the above list and make sure to
-        // prefix "src/" when importing source modules. Would also have to
-        // use the "paths" field to tsconfig.json to help typescript know
-        // what we are doing.
+        extensions: [ ".ts", ], // ".json", ".tsx", 
+        modules: [ PROJECT_ROOT, ], // match tsconfig.baseUrl
     },
     module: {
-        rules: [
+        rules: [ // https://webpack.js.org/loaders/
             {
-                // https://github.com/TypeStrong/ts-loader#loader-options
                 test: /\.tsx?$/,
                 use: {
                     loader: "ts-loader",
@@ -112,13 +102,34 @@ const config: webpack.Configuration = {
                 exclude: /node_modules/,
             },
             // TODO: look into need for html loader
-        ] as Array<webpack.RuleSetRule>,
+        ],
     },
-    plugins,
     output: {
-        path: path.resolve(context, "dist"),
-        filename: "[name]/index.[contentHash].js",
+        path: path.resolve(PROJECT_ROOT, "dist"),
+        filename: "[name]/index.js",
+        sourcePrefix: WATERMARK,
     },
-};
+}; };
 
-module.exports = config;
+
+
+/**
+ * Add entrypoints for webpages:
+ */
+const webConfig = BaseConfig();
+(<const>[ /* TODO: "homepage", */ "offline", "client", ]).forEach((name) => {
+    webConfig.entry[name] = `./src/${name}/index.ts`;
+    // entry[`${name}_body`] = `./src/${name}/body.html`;
+    webConfig.plugins.push(
+        new HtmlPlugin({
+            template: "./src/base/index.html",
+            filename: `${name}/index.html`,
+            chunks: [ name, /*`${name}_body`,*/ ],
+            hash: true,
+        })
+    );
+});
+
+
+
+module.exports = [ webConfig, ];
