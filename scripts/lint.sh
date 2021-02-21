2a43927ebac75d7d1b2895f08ea866a82e035737
david-fong <davidfong19@gmail.com>
2021-02-21 13:24:30 -0800

    clean up build scripts

--- scripts/lint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-time pnpx --no-install eslint --color --cache --cache-location './node_modules/.cache/eslint-cache.json' "${@:-./src/}"
--- scripts/server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# This is for development purposes only.
-
-# --frozen-intrinsics \ # cjs >:(
-# --experimental-policy='policy.json' \
-# --disallow-code-generation-from-strings # >:( depd
-# [[ "${BASH_SOURCE[0]}" != "${0}" ]] \
-#     && exec node --enable-source-maps --disable-proto=delete "./dist/server/index.js" \
-#     ||      node --enable-source-maps --disable-proto=delete "./dist/server/index.js" ;
